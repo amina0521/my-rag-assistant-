c2bb732f7da6ac1934c02fccf8ccb3dfc8c62685
amina0521 <aminafaisal2005@gmail.com>
2026-04-20 11:03:05 +0800

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from langchain.document_loaders import TextLoader
+from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
 
